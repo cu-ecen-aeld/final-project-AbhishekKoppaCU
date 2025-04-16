b767c1f9c2d993efbc1fe53d5949ef1cf318c1b8
AbhishekKoppa <abhishekkoppa@Ubuntu22.04-AESD.myguest.virtualbox.org>
2025-04-16 17:31:40 -0600

    add sender and receiver sockets. Not tested.

---
+++ socket/receiver/pinet_receiver_socket.c
@@ -0,0 +1,45 @@
+// udp_to_pinet.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define DEVICE "/dev/pinet"
+
+int main() {
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) { perror("socket"); exit(1); }
+
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind"); exit(1);
+    }
+
+    int dev_fd = open(DEVICE, O_WRONLY);
+    if (dev_fd < 0) {
+        perror("open /dev/pinet"); exit(1);
+    }
+
+    printf("Listening on UDP port %d...\n", PORT);
+
+    char buf[128];
+    while (1) {
+        ssize_t len = recvfrom(sockfd, buf, sizeof(buf)-1, 0, NULL, NULL);
+        if (len > 0) {
+            buf[len] = '\0';
+            write(dev_fd, buf, len);  // write to your char driver
+        }
+    }
+
+    close(dev_fd);
+    close(sockfd);
+    return 0;
+}
+
---
+++ socket/sender/pinet_sender_socket.c
@@ -0,0 +1,33 @@
+// udp_sender.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+#define DEST_IP "192.168.1.10"  // IP of RPi #2
+#define DEST_PORT 12345
+
+int main() {
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) { perror("socket"); exit(1); }
+
+    struct sockaddr_in dest = {0};
+    dest.sin_family = AF_INET;
+    dest.sin_port = htons(DEST_PORT);
+    inet_pton(AF_INET, DEST_IP, &dest.sin_addr);
+
+    char msg[64];
+    int counter = 0;
+
+    while (1) {
+        snprintf(msg, sizeof(msg), "sensor_data_%d", counter++);
+        sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr*)&dest, sizeof(dest));
+        usleep(10000);  // 10ms = 10,000 µs
+    }
+
+    close(sockfd);
+    return 0;
+}
+
