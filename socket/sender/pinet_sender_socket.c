be6d6b6eab9d3bb96be4a022acd00b45b8bd4dc7
AbhishekKoppa <abhishekkoppa@Ubuntu22.04-AESD.myguest.virtualbox.org>
2025-04-24 22:58:01 -0600

    Implement the sender socket as a daemon which keeps querying the /dev/pinet for new data and transmits to the dest IP using a UDP socket. Also has a new parameter added <interface>. User has to give 2 parameters - dest IP and the interface to use (usually ens0p3 on VM and wlan0 on RPi).

--- socket/sender/pinet_sender_socket.c
+++ socket/sender/pinet_sender_socket.c
@@ -3,37 +3,88 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/if.h>
 
-#define DEST_PORT 12345  // Fixed port
+#define DEST_PORT 12345
+#define DEVICE "/dev/pinet"
+#define BUF_SIZE 64
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <Destination_IP>\n", argv[0]);
-        exit(1);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <Destination_IP> <Interface_Name>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    const char* dest_ip = argv[1];
+    const char *dest_ip = argv[1];
+    const char *iface = argv[2];
 
+    // Open character device for reading
+    int dev_fd = open(DEVICE, O_RDONLY);
+    if (dev_fd < 0) {
+        perror("Failed to open /dev/pinet");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create UDP socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) { perror("socket"); exit(1); }
+    if (sockfd < 0) {
+        perror("socket");
+        close(dev_fd);
+        exit(EXIT_FAILURE);
+    }
 
+    // Bind socket to the specified interface
+    if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, iface, strlen(iface)) < 0) {
+        perror("setsockopt(SO_BINDTODEVICE)");
+        close(sockfd);
+        close(dev_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Set destination address
     struct sockaddr_in dest = {0};
     dest.sin_family = AF_INET;
     dest.sin_port = htons(DEST_PORT);
     if (inet_pton(AF_INET, dest_ip, &dest.sin_addr) <= 0) {
-        perror("Invalid IP address"); exit(1);
+        perror("Invalid IP address");
+        close(sockfd);
+        close(dev_fd);
+        exit(EXIT_FAILURE);
     }
 
-    char msg[64];
-    int counter = 0;
+    printf("Sender daemon started. Interface: %s, Destination: %s\n", iface, dest_ip);
 
     while (1) {
-        snprintf(msg, sizeof(msg), "sensor_data_%d", counter++);
-        sendto(sockfd, msg, strlen(msg), 0, (struct sockaddr*)&dest, sizeof(dest));
-        usleep(10000);  // 10ms
+        char buffer[BUF_SIZE] = {0};
+
+        ssize_t len = read(dev_fd, buffer, sizeof(buffer));
+        if (len < 0) {
+            perror("read from /dev/pinet");
+            break;
+        } else if (len == 0) {
+            usleep(10000);
+            continue;
+        }
+
+        sendto(sockfd, buffer, len, 0, (struct sockaddr*)&dest, sizeof(dest));
+
+        // Print payload as integer (assumes 4-byte int in network order)
+        int value = 0;
+        if (len >= 4) {
+            memcpy(&value, buffer, 4);
+            printf("Sent %ld bytes to %s:%d | Payload = %d\n", len, dest_ip, DEST_PORT, value);
+        } else {
+            printf("Sent %ld bytes to %s:%d | (Payload too short to parse as int)\n", len, dest_ip, DEST_PORT);
+        }
+
     }
 
     close(sockfd);
+    close(dev_fd);
     return 0;
 }
+
