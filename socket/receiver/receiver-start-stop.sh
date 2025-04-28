cfc3c32333de18369b4eaa812995fec37c31bc0c
Nalin <nalin@Nalin.myguest.virtualbox.org>
2025-04-28 16:41:41 -0600

    adding start stop scripts for socket

---
+++ socket/receiver/receiver-start-stop.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+case "$1" in
+  start)
+    echo "Starting receiver"
+    /usr/bin/pinet_receiver &
+    ;;
+  stop)
+    echo "Stopping VNCL daemon"
+    killall pinet_receiver
+    ;;
+  restart)
+    $0 stop
+    $0 start
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+esac
+
+exit 0
